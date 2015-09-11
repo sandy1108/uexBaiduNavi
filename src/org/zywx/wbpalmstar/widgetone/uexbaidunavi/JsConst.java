a462283465383a6856e307dd356c09601d845866
yanlongtao <longtao.yan@3g2win.com>
2015-09-11 14:06:38 +0800

    更新百度导航SDK

---
+++ src/org/zywx/wbpalmstar/widgetone/uexbaidunavi/JsConst.java
@@ -0,0 +1,8 @@
+package org.zywx.wbpalmstar.widgetone.uexbaidunavi;
+
+public class JsConst {
+
+    public static final String CALLBACK_START_ROUTE_PLAN = "uexBaiduNavi.cbStartRoutePlan";
+    public static final String ON_EXIT_NAVI = "uexBaiduNavi.onExitNavi";
+    public static final String CALLBACK_INIT="uexBaiduNavi.cbInit";
+}
